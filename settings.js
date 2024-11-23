12ec3eec85f0b2e0917be78b9577599899dc93bf
Uknown <ferdinandkamagi@gmail.com>
2024-11-23 14:24:32 +0800

    new bot again

---
+++ settings.js
@@ -0,0 +1,13 @@
+// 𝗖𝗢𝗡𝗧𝗥𝗢𝗟 𝗕𝗢𝗧 
+// BAHAN BUG/TES SC KE NO INI 6282295167424
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['639813411437'] //OWNER
+global.botname = 'Cixzyy' //BOT NAME
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+//global.baileys2 = require("@adiwajshing/baileys") // NOT CHANGE
+global.creator = "Silfy" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Silfy" //OPSIONAL
+global.wm = "Silfy" // OPSIONAL
+global.idCH = "..." // OPSIONAL
